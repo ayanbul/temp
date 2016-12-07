2738442dc3df5b620eb071cfb2a8eb20c854d4d1
ayanbul <ayanbul@umass.edu>
2016-12-06 22:05:13 -0500

    update

--- test.py
+++ test.py
@@ -2,13 +2,16 @@
 import webapp2
 #from google.appengine.ext.webapp2.util import run_wsgi_app
 
+
 class Cron(webapp2.RequestHandler):
     def get(self):
-        print "Hello\n"
+        self.response.write('Hello')
 
 class Hello(webapp2.RequestHandler):
     def get(self):
-        print "Hello, World!"
+        self.response.write('Hello, World!')
+
+
 
 app = webapp2.WSGIApplication([('/', Hello), ('/tasks/data', Cron)], debug=True)
 
