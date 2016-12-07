91c6600055e6a58d05382b6f5368d1513426ca77
ayanbul <ayanbul@umass.edu>
2016-12-06 22:45:11 -0500

    added another cron

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import webapp2
 #from google.appengine.ext.webapp2.util import run_wsgi_app
 
 
-class Cron(webapp2.RequestHandler):
+class Hello1Min(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello\n')
 
@@ -11,9 +11,13 @@ class Hello(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, World!')
 
+class Goodbye2Min(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Goodbye\n')
 
 
-app = webapp2.WSGIApplication([('/', Hello), ('/tasks/data', Cron)], debug=True)
+
+app = webapp2.WSGIApplication([('/', Hello), ('/hello', Hello1Min), ('/goodbye', Goodbye2Min)], debug=True)
 
 #if __name__ == '__main__':
 #    run_wsgi_app(application)
