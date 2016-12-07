4f31f5389aa322115f055c909b38f7c62230e58f
ayanbul <ayanbul@umass.edu>
2016-12-06 23:22:13 -0500

    update

--- test.py
+++ test.py
@@ -12,9 +12,12 @@ class Hello(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, World!')
 
+
 class Goodbye2Min(webapp2.RequestHandler):
     def get(self):
         self.response.write(i)
+        i = i + 1
+
 
 
 
@@ -26,7 +29,6 @@ def main():
     run_wsgi_app(application)
 
 if __name__ == '__main__':
-    i = i+1
     run_wsgi_app(application)
 
 
