14474983a11851bd1273e6ad85a893b0bd4f0397
ayanbul <ayanbul@umass.edu>
2016-12-06 20:54:01 -0500

    update

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp2
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class ParseXMLHandler(webapp.RequestHandler):
+class TestHandler(webapp2.RequestHandler):
     def get(self):
         print "Hello\n"
 
-application = webapp.WSGIApplication([('/tasks/data', ParseXMLHandler)], debug=True)
+application = webapp2.WSGIApplication([('/tasks/data', TestHandler)], debug=True)
 
 if __name__ == '__main__':
-    print "Hello World"
+    print "Hello World\n"
     run_wsgi_app(application)
 
 def main():
