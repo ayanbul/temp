b5d8ae6e5aaa19705aad7da69692ea8d5aa6f7f4
ayanbul <ayanbul@umass.edu>
2016-12-08 19:40:55 -0500

    comment from fire...

--- test.py
+++ test.py
@@ -1,36 +1,36 @@
 import json
 import time
 import webapp2
-from firebase import firebase, jsonutil
+#from firebase import firebase, jsonutil
 
 
 class DailyMaintanence(webapp2.RequestHandler):
     def get(self):
         self.response.write('Script Delete')
-#        fb = firebase.FirebaseApplication('https://skwad-729af.firebaseio.com', None)
-#            packs = fb.get('/packs', None)
-#            
-#            if packs is not None:
-#                for location in packs:
-#                    for pack_id in packs[location]:
-#                        pack = packs[location][pack_id]
-#                        
-#                        print "Pack Name:", pack['name']
-#                        print "Description:", pack['desc']
-#                        print "Lat:", pack['lat']
-#                        print "Lon:", pack['lon']
-#                        print "Time:", pack['time']
-#                        
-#                        t_delta = time.time()-float(pack['time'])
-#                        t_remain = 86400-t_delta
-#                        
-#                        if t_remain <= 0:
-#                            fb.delete("/".join(["packs", location]), pack_id)
-#                            print "Pack deleted."
-#                        else:
-#                            print "Pack has %i seconds remaining." % t_remain
-#
-#                    print "------------------------------------------"
+        fb = firebase.FirebaseApplication('https://skwad-729af.firebaseio.com', None)
+            packs = fb.get('/packs', None)
+            
+            if packs is not None:
+                for location in packs:
+                    for pack_id in packs[location]:
+                        pack = packs[location][pack_id]
+                        
+                        print "Pack Name:", pack['name']
+                        print "Description:", pack['desc']
+                        print "Lat:", pack['lat']
+                        print "Lon:", pack['lon']
+                        print "Time:", pack['time']
+                        
+                        t_delta = time.time()-float(pack['time'])
+                        t_remain = 86400-t_delta
+                        
+                        if t_remain <= 0:
+                            fb.delete("/".join(["packs", location]), pack_id)
+                            print "Pack deleted."
+                        else:
+                            print "Pack has %i seconds remaining." % t_remain
+
+                    print "------------------------------------------"
 
 
 class Main(webapp2.RequestHandler):
