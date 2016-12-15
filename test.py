9bf844d9073e97f5e864ba7af275746c613c3559
ayanbul <ayanbul@umass.edu>
2016-12-14 21:29:42 -0500

    fixed indentations

--- test.py
+++ test.py
@@ -11,28 +11,28 @@ class DailyMaintanence(webapp2.RequestHandler):
     def get(self):
         self.response.write('Script Delete')
         fb = firebase.FirebaseApplication('https://test-50acf.firebaseio.com', None)
-            packs = fb.get('/packs', None)
+        packs = fb.get('/packs', None)
             
-            if packs is not None:
-                for location in packs:
-                    for pack_id in packs[location]:
-                        pack = packs[location][pack_id]
-                        
-                        print "Pack Name:", pack['name']
-                        print "Description:", pack['desc']
-                        print "Lat:", pack['lat']
-                        print "Lon:", pack['lon']
-                        print "Time:", pack['time']
-                        
-                        t_delta = time.time()-float(pack['time'])
-                        t_remain = 86400-t_delta
-                        
-                        if t_remain <= 0:
-                            fb.delete("/".join(["packs", location]), pack_id)
-                            print "Pack deleted."
-                        else:
-                            print "Pack has %i seconds remaining." % t_remain
-
+        if packs is not None:
+            for location in packs:
+                for pack_id in packs[location]:
+                    pack = packs[location][pack_id]
+                    
+                    print "Pack Name:", pack['name']
+                    print "Description:", pack['desc']
+                    print "Lat:", pack['lat']
+                    print "Lon:", pack['lon']
+                    print "Time:", pack['time']
+                    
+                    t_delta = time.time()-float(pack['time'])
+                    t_remain = 86400-t_delta
+                    
+                    if t_remain <= 0:
+                        fb.delete("/".join(["packs", location]), pack_id)
+                        print "Pack deleted."
+                    else:
+                        print "Pack has %i seconds remaining." % t_remain
+                    
                     print "------------------------------------------"
 
 
